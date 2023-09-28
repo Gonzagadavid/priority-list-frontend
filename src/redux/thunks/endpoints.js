fd6cfa612c911a8ee8cea440fe0a8d719c735ba8
gonzagadavid <david.blesses@hotmail.com>
2023-09-27 23:36:59 -0300

    refactor: add endpoint as  environment variable

--- src/redux/thunks/endpoints.js
+++ src/redux/thunks/endpoints.js
@@ -1,19 +1,9 @@
-// endpoints para produção, para rodar localmente comente o endpoints abaixo
+const BASE_URL = process.env.REACT_APP_BACKENDAPP || 'http://localhost:3800';
 
-export const LOGIN = 'https://priority-list-backend-production.up.railway.app/user/login';
+export const LOGIN = `${BASE_URL}/user/login`;
 
-export const SIGNUP = 'https://priority-list-backend-production.up.railway.app/user';
+export const SIGNUP = `${BASE_URL}/user`;
 
-export const GET_TASKS = 'https://priority-list-backend-production.up.railway.app/task';
+export const GET_TASKS = `${BASE_URL}/task`;
 
-export const TASK_BY_ID = (id) => `https://priority-list-backend-production.up.railway.app/task/${id}`;
-
-// para rodar a aplicação localmente descomente os endpoints abaixo
-
-// export const LOGIN = 'http://localhost:3800/user/login';
-
-// export const SIGNUP = 'http://localhost:3800/user';
-
-// export const GET_TASKS = 'http://localhost:3800/task';
-
-// export const TASK_BY_ID = (id) => `http://localhost:3800/task/${id}`;
+export const TASK_BY_ID = (id) => `${BASE_URL}/task/${id}`;
